c3057afb5dd4b8ead7ade7b67481197bd3981a99
leeboa2005 <leeboa2005@naver.com>
2024-05-30 11:14:12 +0900

    ex2 과제 제출

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,14 @@
-import { memo } from "react";
+import { memo } from 'react';
 
 const List = ({ items }) => {
-  console.log("List component rendered");
-  return (
-    <ul>
-      {items.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
-  );
+    console.log('List component rendered');
+    return (
+        <ul>
+            {items.map((item, index) => (
+                <li key={index}>{item}</li>
+            ))}
+        </ul>
+    );
 };
 
 export default memo(List);
